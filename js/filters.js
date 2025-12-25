1fe298b914419cfbe13aee9fac1fcd6224686c29
maria-antonova616 <aatheist616.ph@gmail.com>
2025-12-25 22:11:05 +0500

    Перламутровые пуговицы

---
+++ js/filters.js
@@ -0,0 +1,52 @@
+const Filter = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed',
+};
+
+const filterElement = document.querySelector('.img-filters');
+let currentFilter = Filter.DEFAULT;
+let pictures = [];
+
+const sortRandomly = () => Math.random() - 0.5;
+
+const sortByComments = (pictureA, pictureB) =>
+  pictureB.comments.length - pictureA.comments.length;
+
+const getFilteredPictures = () => {
+  switch (currentFilter) {
+    case Filter.RANDOM:
+      return [...pictures].sort(sortRandomly).slice(0, 10);
+    case Filter.DISCUSSED:
+      return [...pictures].sort(sortByComments);
+    default:
+      return [...pictures];
+  }
+};
+
+const setOnFilterClick = (cb) => {
+  filterElement.addEventListener('click', (evt) => {
+    if (!evt.target.classList.contains('img-filters__button')) {
+      return;
+    }
+
+    const clickedButton = evt.target;
+    if (clickedButton.id === currentFilter) {
+      return;
+    }
+
+    filterElement.querySelector('.img-filters__button--active')
+      .classList.remove('img-filters__button--active');
+    clickedButton.classList.add('img-filters__button--active');
+    currentFilter = clickedButton.id;
+    cb(getFilteredPictures());
+  });
+};
+
+const initFilters = (loadedPictures, cb) => {
+  filterElement.classList.remove('img-filters--inactive');
+  pictures = [...loadedPictures];
+  setOnFilterClick(cb);
+};
+
+export { initFilters, getFilteredPictures };
--- js/util.js
+++ js/util.js
@@ -34,5 +34,24 @@ const createComment = () => {
   };
 };
 
-export { getRandomInteger, getRandomArrayElement, createIdGenerator, createComment };
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+  return (...rest) => {
+    const now = new Date();
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export { getRandomInteger, getRandomArrayElement, createIdGenerator, createComment, debounce, throttle };
 
